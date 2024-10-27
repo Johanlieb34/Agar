419545c72e0aa68fb6148e89f82227703b3306be
Johanlieb34 <godswillosamaye810@gmail.com>
2024-10-27 11:28:30 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("basicBtn").addEventListener("click", function() {
-    var example = document.getElementById("basicExample");
-    if (example.style.display === "none") {
-        example.style.display = "block";
-    } else {
-        example.style.display = "none";
-    }
-});
